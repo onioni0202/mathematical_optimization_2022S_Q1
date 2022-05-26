921fdfd007046036b29b69f215a1246ca4cb3b4e
onioni0202 <49858062+onioni0202@users.noreply.github.com>
2022-05-26 12:18:05 +0900

    fixed all algorithms

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from ex1 import ex1
 from ex2 import ex2
 from ex3 import ex3
 
-if __name__ == '__main__':
+
+def main():
+    # ex1()
+    # ex2()
     ex3()
+
+
+if __name__ == '__main__':
+    main()
